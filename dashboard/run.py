30f5df4335443babd8e1ff6740484dfa8f3215aa
CrisFeliciano <felicianocristiano@hotmail.com>
2021-07-22 18:48:56 -0300

    Primeiros arquivos

---
+++ dashboard/run.py
@@ -0,0 +1,10 @@
+import flask as f
+import dashboard
+
+app = f.Flask(__name__)
+
+app.register_blueprint(dashboard.jenkins.blueprint.bp)
+app.register_blueprint(dashboard.docker.blueprint.bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
